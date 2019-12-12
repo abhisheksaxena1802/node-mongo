d370080b76946ad424b14f612d72919e5504d2b1
Satnam Singh <Satnam.Singh156@T-Mobile.com>
2019-12-12 16:06:17 +0530

    commit

--- route/route.js
+++ route/route.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send({"name": "satnam"})
+router.get('/user', async (req, res) => {
+  const dbReference = req.app.locals.db.collection("user");
+  try {
+    const users = await dbReference.find().toArray();
+    if (users.length === 0) {
+      res.status(500).send({ "message": "No user found" });
+    } else {
+      res.status(200).send(users)
+    }
+  } catch (error) {
+    res.status(400).send({ error: error })
+  }
+
+  router.post('/user', async (req, res) => {
+    const dbReference = req.app.locals.db.collection("user");
+    try {
+      const body = {
+        "username": req.body.username,
+        "password": req.body.password
+      }
+      const pushUser = await dbReference.insertOne(body);
+      res.status(200).send({ message: "user created" })
+
+    } catch (error) {
+      res.status(400).send({ error: error })
+    }
+
+  })
+
+
 })
 
 module.exports = router;
